90527df749bf0b0f546c7f5f15edc873b229dbd5
CaryJudson <xboxfan123@gmail.com>
2021-05-02 13:58:45 -0400

    Bugged table.

--- finalproject/collegesearch/college_data/college_soup.py
+++ finalproject/collegesearch/college_data/college_soup.py
@@ -10,20 +10,18 @@ names = []
 attend = []
 price = []
 sat = []
-for x in soup.select(".card__inner"):
-    for y in x.select(".search-result__title"):
-        names.append(y.text)
-    for z in x.select(".search-result-fact"):
-        for q in z.select(".search-result-fact__label"):
-            if q.text == "Acceptance Rate":
-                feature = attend
-            if q.text == "Net Price":
-                feature = price
-            if q.text == "SAT Range":
-                feature = sat
-                for w in z.select(".search-result-fact__value"):
-                    feature.append(w.text)
-df = pd.DataFrame(np.array([[names], [attend], [price], [sat]]),
-
-                   columns=['Name', 'Acceptance Rate', 'Net Price', 'SAT Range'])
-
+for x in soup.select(".card"):
+    if x.find(string="Sponsored") is not None:
+        for y in x.select(".search-result__title"):
+            names.append(y.text)
+        for z in x.select(".search-result-fact"):
+            for q in z.select(".search-result-fact__label"):
+                if q.text == "Acceptance Rate":
+                    feature = attend
+                if q.text == "Net Price":
+                    feature = price
+                if q.text == "SAT Range":
+                    feature = sat
+                    for w in z.select(".search-result-fact__value"):
+                        feature.append(w.text)
+    print(names)
