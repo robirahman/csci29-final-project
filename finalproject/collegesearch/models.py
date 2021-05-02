e34a48a8453974dd82b9b9cbf3b47ae171c22625
robirahman <robirahman94@gmail.com>
2021-05-02 15:13:11 -0400

    feat: implement django models for colleges, states, regions, and site users

--- finalproject/collegesearch/models.py
+++ finalproject/collegesearch/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import *
 
-# Create your models here.
+
+class User(AbstractUser):
+    def __str__(self):
+        return str(self.username)
+
+
+class College(Model):
+    def __str__(self):
+        return str(self.name)
+
+    name = CharField()
+    id = PositiveSmallIntegerField(primary_key=True)
+    population = PositiveIntegerField()
+    description = TextField()
+    state = ForeignKey("State", on_delete=PROTECT, related_name="located_here")
+    sat_min = PositiveSmallIntegerField()
+    sat_max = PositiveSmallIntegerField()
+    tuition = PositiveIntegerField()
+    tuition_in_state = PositiveIntegerField()
+    rating = PositiveSmallIntegerField()
+    public = BooleanField()
+
+
+class State(Model):
+    name = CharField()
+    id = PositiveSmallIntegerField(primary_key=True)
+    region = ForeignKey("Region", on_delete=SET_NULL)
+
+
+class Region(Model):
+    name = CharField()
+    id = PositiveSmallIntegerField(primary_key=True)
+    neighbors = ForeignKey("Region", on_delete=SET_NULL)
--- finalproject/finalproject/urls.py
+++ finalproject/finalproject/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('collegesearch/', include('collegesearch.urls'))
+    path("admin/", admin.site.urls),
+    path("collegesearch/", include("collegesearch.urls")),
 ]
