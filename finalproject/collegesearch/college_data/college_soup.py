323bbec0dd6628337f13f80195c35b5a26e91a28
CaryJudson <xboxfan123@gmail.com>
2021-05-03 09:48:51 -0400

    added wikipedia

--- finalproject/collegesearch/college_data/college_soup.py
+++ finalproject/collegesearch/college_data/college_soup.py
@@ -3,6 +3,7 @@ import codecs
 import pandas as pd
 import numpy as np
 import re
+import wikipedia
 html = codecs.open("niche_college_list_1.html")
 soup = BeautifulSoup(html, 'html.parser')
 
@@ -10,10 +11,13 @@ names = []
 attend = []
 price = []
 sat = []
+wiki_list = []
 for x in soup.select(".card"):
-    if x.find(string="Sponsored") is not None:
+    if x.find(string="Sponsored") is None:
         for y in x.select(".search-result__title"):
             names.append(y.text)
+            wiki = wikipedia.page(y.text).content
+            wiki_list.append(wiki)
         for z in x.select(".search-result-fact"):
             for q in z.select(".search-result-fact__label"):
                 if q.text == "Acceptance Rate":
@@ -24,4 +28,4 @@ for x in soup.select(".card"):
                     feature = sat
                     for w in z.select(".search-result-fact__value"):
                         feature.append(w.text)
-    print(names)
+print(wiki_list)
