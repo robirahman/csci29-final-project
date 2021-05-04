cf12a0ef97e7419d62b6563e2a4002f21efd7ae9
CaryJudson <xboxfan123@gmail.com>
2021-05-04 11:17:22 -0400

    added word2vec model and vectors

--- finalproject/collegesearch/college_data/college_soup.py
+++ finalproject/collegesearch/college_data/college_soup.py
@@ -1,9 +1,9 @@
 from bs4 import BeautifulSoup
 import codecs
-import pandas as pd
-import numpy as np
-import re
 import wikipedia
+from gensim.models import Word2Vec
+import re
+import numpy as np
 html = codecs.open("niche_college_list_1.html")
 soup = BeautifulSoup(html, 'html.parser')
 
@@ -16,7 +16,10 @@ for x in soup.select(".card"):
     if x.find(string="Sponsored") is None:
         for y in x.select(".search-result__title"):
             names.append(y.text)
-            wiki = wikipedia.page(y.text).content
+            search = wikipedia.search(y.text)[0]
+            if search == "Northwestern University":
+                search = "north western university"
+            wiki = wikipedia.page(search).content
             wiki_list.append(wiki)
         for z in x.select(".search-result-fact"):
             for q in z.select(".search-result-fact__label"):
@@ -28,4 +31,15 @@ for x in soup.select(".card"):
                     feature = sat
                     for w in z.select(".search-result-fact__value"):
                         feature.append(w.text)
-print(wiki_list)
+def tokenize(text):
+    """Get all "words", including contractions
+    Example::
+        tokenize("Hello, I'm Scott") --> ['hello', "i'm", 'scott']
+    :returns: list of words in statement.
+    :rtype: list
+    """
+    return re.findall(r"\w[\w']+", text.lower())
+tokenized_sentences = np.array(list(map(tokenize,wiki_list)))
+model = Word2Vec(tokenized_sentences, window=2, min_count=0)
+words = model.wv.key_to_index()
+we_dict = {word:model.wv[word] for word in words}
