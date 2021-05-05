f9a259ea7c171c057afa832d6b2183b35d6e15a8
robirahman <robirahman94@gmail.com>
2021-05-05 16:49:32 -0400

    feat: create web form for college search app

--- finalproject/collegesearch/views.py
+++ finalproject/collegesearch/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the college search homepage.")
+    return render(request, "collegesearch/index.html", {"colleges": None})
