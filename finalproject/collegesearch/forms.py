d7d840d84093d0dbf2abcfcc7ec24669780697b8
robirahman <robirahman94@gmail.com>
2021-05-06 15:34:42 -0400

    feat: implement web form submission and scoring algorithm

---
+++ finalproject/collegesearch/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class CollegeSearchForm(forms.Form):
+    pass
--- finalproject/collegesearch/views.py
+++ finalproject/collegesearch/views.py
@@ -2,7 +2,29 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .utils import top_matches, embed_description
+
 
 def index(request):
-    return render(request, "collegesearch/index.html", {"colleges": None})
+    return render(request, "collegesearch/index.html")
 
+def results(request):
+    if request.method == "POST":
+        description = request.POST["description"]
+        vector = embed_description(description)
+        size = int(request.POST["size"])
+        sat_verbal = int(request.POST["sat_verbal"])
+        sat_math = int(request.POST["sat_math"])
+        public = int(request.POST["public"])
+        preferences = {
+            "description": description,
+            "vector": vector,
+            "size": size,
+            "sat_verbal": sat_verbal,
+            "sat_math": sat_math,
+            "public": public
+        }
+    return render(request, "collegesearch/results.html", {
+        "preferences": preferences,
+        "colleges": top_matches(preferences)
+    })
