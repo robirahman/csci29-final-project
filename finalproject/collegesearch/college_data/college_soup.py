88a1c0353804b04e9666224051c0238f8a73a918
robirahman <robirahman94@gmail.com>
2021-05-09 17:05:48 -0400

    fix: add new college_soup.py which was overwritten by branch merge

--- finalproject/collegesearch/college_data/college_soup.py
+++ finalproject/collegesearch/college_data/college_soup.py
@@ -10,10 +10,14 @@ from embedding import WordEmbedding
 from prefect.agent.local import LocalAgent
 import csv
 import os
+from prefect.engine.results import LocalResult
+from prefect.engine.serializers import PandasSerializer
 
 
-@task(log_stdout=True, nout=3)
-def college_facts():
+@task(log_stdout=True,nout=4,result=LocalResult(serializer=PandasSerializer(file_type='csv'),dir='./',location="facts.csv"))
+def college_facts(bypass):
+    if bypass:
+        return pd.read_csv('./college_data.csv')
     html = codecs.open("niche_college_list_1.html")
     soup = BeautifulSoup(html, "html.parser")
     names = []
@@ -95,39 +99,81 @@ def college_facts():
                         if q.text == "SAT Range":
                             sat_min.append(w.text[0:4])
                             sat_max.append(w.text[-4:])
-    data = {
-        "sat_min": sat_min,
-        "sat_max": sat_max,
-        "names": names,
-        "price": price,
-        "acceptance": attend,
-        "size": size,
-        "state": state,
-    }
-    college_facts = pd.DataFrame(data=data)
-    college_facts.to_csv("facts.csv")
-    return wiki_list, names, college_facts
+                for z in x.select(".search-result-fact"):
+                    for q in z.select(".search-result-fact__label"):
+                        for w in z.select(".search-result-fact__value"):
+                            if q.text == "Acceptance Rate":
+                                attend.append(w.text)
+                            if q.text == "Net Price":
+                                price.append(w.text)
+                            if q.text == "SAT Range":
+                                sat_min.append(w.text[0:4])
+                                sat_max.append(w.text[-4:])
+        data = {'sat_min': sat_min, 'sat_max': sat_max, 'names': names, 'price': price, 'acceptance': attend,
+                'size': size}
+        college_facts = pd.DataFrame(data=data)
+        return college_facts
 
+    @task(log_stdout=True, nout=4,
+          result=LocalResult(serializer=PandasSerializer(file_type='csv'), dir='./', location="wiki_list.csv"))
+    def create_wiki(facts, bypass):
+        if bypass:
+            return pd.read_csv('./wiki_list.csv')
+        names = facts['names'].to_list()
+        wiki_list = []
+        for search in names:
+            if search == 'William & Mary':
+                search = 'College of William & Mary'
+            if search == "Northwestern University":
+                search = "north western university"
+            elif search == "Bowdoin College":
+                search = 'bowden college'
+            elif search == "Williams College":
+                search = 'william college'
+            elif search == 'Northeastern University':
+                search = 'north western university'
+            elif search == 'Kenyon College':
+                search = 'kenyan college'
+            wiki = wikipedia.page(search).content
+            wiki_list.append(wiki)
+        data = {'wiki': wiki_list}
+        wiki_list = pd.DataFrame(data=data)
+        return wiki_list
 
-@task(log_stdout=True, nout=3)
-def college_embeddings(wiki_list, names):
-    tokenized_sentences = np.array(list(map(WordEmbedding.tokenize, wiki_list)))
-    model = Word2Vec(
-        tokenized_sentences, window=2, min_count=0, vector_size=100, workers=4
-    )
-    words = model.wv.key_to_index
-    we_dict = {word: model.wv[word] for word in words}
-    embedding = WordEmbedding(we_dict)
-    embeddings = np.array(list(map(embedding.embed_document, wiki_list)))
-    college_embedding = pd.DataFrame(embeddings, names)
-    college_embedding.to_csv("embedding.csv")
-    return college_embedding
+    @task(log_stdout=True, nout=4,
+          result=LocalResult(serializer=PandasSerializer(file_type='csv'), dir='./', location="embedding_dict.csv"))
+    def create_dict(wiki_list, bypass):
+        if bypass:
+            return pd.read_csv('./embedding_dict.csv')
+        wiki_list = wiki_list['wiki'].to_list()
+        tokenized_sentences = np.array(list(map(WordEmbedding.tokenize, wiki_list)))
+        model = Word2Vec(tokenized_sentences, window=2, min_count=0, vector_size=100, workers=4)
+        words = model.wv.key_to_index
+        we_dict = {word: model.wv[word] for word in words}
+        return pd.DataFrame(data=we_dict)
 
+    @task(log_stdout=True, nout=4,
+          result=LocalResult(serializer=PandasSerializer(file_type='csv'), dir='./', location="college_embeddings.csv"))
+    def college_embeddings(we_dict, wiki_list, facts, bypass):
+        if bypass:
+            return pd.read_csv('./college_embeddings.csv')
+        names = facts['names'].to_list()
+        wiki_list = wiki_list['wiki'].to_list()
+        tokenized_sentences = np.array(list(map(WordEmbedding.tokenize, wiki_list)))
+        model = Word2Vec(tokenized_sentences, window=2, min_count=0, vector_size=100, workers=4)
+        words = model.wv.key_to_index
+        we_dict = {word: model.wv[word] for word in words}
+        embedding = WordEmbedding(we_dict)
+        embeddings = np.array(list(map(embedding.embed_document, wiki_list)))
+        college_embedding = pd.DataFrame(embeddings, names)
+        return college_embedding
 
-with Flow("data analysis") as flow:
-    wiki_list, names, college_facts = college_facts()
-    college_embedding = college_embeddings(wiki_list, names)
-
-flow.register(project_name="college")
-# LocalAgent().start()
-flow.run()
+    with Flow("data analysis") as flow:
+        bypass = Parameter("bypass", default=False, required=False)
+        college_fact = college_facts(bypass=bypass)
+        wiki_list = create_wiki(college_fact, bypass=bypass)
+        dict = create_dict(wiki_list, bypass=bypass)
+        college_embedding = college_embeddings(dict, wiki_list, college_fact, bypass=bypass)
+    flow.register(project_name="college")
+    # LocalAgent().start()
+    flow.run(bypass=False)
