07bea0481e38a7abe5e91df25521823d405bc900
CaryJudson <xboxfan123@gmail.com>
2021-05-11 18:12:28 -0400

    refactor docs again

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), "", "../src"))
-sys.path.insert(0, basedir)
+sys.path.insert(0, os.path.abspath("../.."))
+
 
 
 # -- Project information -----------------------------------------------------
